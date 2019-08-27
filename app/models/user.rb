69b396d2b4d63bac5212bdd90a3882cd8db01126
ben <benedikt.rudolf@gmx.net>
2019-08-27 13:07:21 +0200

    Add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :username, presence:true,
              uniqueness: {case_sensitive: false},
@@ -10,5 +10,4 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
   has_secure_password
-
 end
