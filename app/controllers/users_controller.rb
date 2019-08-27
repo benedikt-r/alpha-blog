395c9a830fd013a95589ade2b231015d31431ac3
ben <benedikt.rudolf@gmx.net>
2019-08-27 11:48:59 +0200

    Add signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to my blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -33,7 +34,6 @@ class UsersController < ApplicationController
   end
 
    def show
-
      @user_articles = @user.articles.paginate(page: params[:page], per_page:5)
   end
 
